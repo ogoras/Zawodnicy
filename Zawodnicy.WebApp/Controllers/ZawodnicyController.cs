98e90ef3a6cae68d78fd8f24584ce2c05c421a4c
ogoras <skus30@wp.pl>
2021-12-14 11:46:33 +0100

    Zawodnicy View

---
+++ Zawodnicy.WebApp/Controllers/ZawodnicyController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Zawodnicy.WebApp.Models;
+
+namespace Zawodnicy.WebApp.Controllers
+{
+    public class ZawodnicyController : Controller
+    {
+        public IConfiguration Configuration;
+
+        public ZawodnicyController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ContentResult GetApiUrl()
+        {
+            var result = Configuration["RestApiUrl"];
+            return Content(result);
+        }
+
+        private string CN()
+        {
+            return ControllerContext.RouteData.Values["controller"].ToString();
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            string _restpath = GetApiUrl().Content + CN();
+            List<ZawodnikVM> zawodnikList = new List<ZawodnikVM>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(_restpath))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    zawodnikList = JsonConvert.DeserializeObject<List<ZawodnikVM>>(apiResponse);
+                }
+            }
+
+            return View(zawodnikList);
+        }
+    }
+}
---
+++ Zawodnicy.WebApp/Models/ZawodnikVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zawodnicy.WebApp.Models
+{
+    public class ZawodnikVM
+    {
+        public int Id { get; set; }
+        public int IdTrenera { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Kraj { get; set; }
+        public DateTime DataUr { get; set; }
+        public double Wzrost { get; set; }   //wzrost w cm
+        public double Waga { get; set; } //waga w kg
+    }
+}
