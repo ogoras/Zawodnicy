6b9824bf0d493f7f3a0e57a675ce8da3207fe46f
ogoras <skus30@wp.pl>
2022-01-04 20:00:31 +0100

    Login View

---
+++ Zawodnicy.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Zawodnicy.WebApp.Models;
+
+namespace Zawodnicy.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //Logowanie POST
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+
+            if (!ModelState.IsValid)    //blad logowania - nie bledne haslo jako niezgodne z walidacja
+                return View(loginVM);
+
+            //zwraca name usera do zalogowania
+            var user = await _userManager.FindByNameAsync(loginVM.Username);
+
+            if (user != null)
+            {
+                //logowanie
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Niepoprawna nazwa użytkownika lub hasło...");
+
+
+            return View(loginVM);
+        }
+
+    }
+}
---
+++ Zawodnicy.WebApp/Models/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zawodnicy.WebApp.Models
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage="Wymagana nazwa użytkownika")]
+        [Display(Name= "Nazwa użytkownika")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Wymagane hasło")]
+        [Display(Name = "Hasło")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
